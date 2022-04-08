2464b068cb7ab8f32e6dff6702f67abe78a29566
Dyego79 <95590936+Dyego79@users.noreply.github.com>
2022-04-07 22:13:32 -0300

    Primer Entrega

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+function App() {
+    return "MI PRIMER PROYECTO REACTJS"
+}
+ReactDOM.render( <App/> , document.getElementById("root"))
+export default App;
